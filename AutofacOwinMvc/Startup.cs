b1f9631bfd618b0a9bc07ca1190b3626ae1ade2c
Travis Illig <tillig@paraesthesia.com>
2014-08-26 10:50:51 -0700

    Fixed issues with OWIN setup.
    See comments in the code to understand what was changed and why.

--- AutofacOwinMvc/Startup.cs
+++ AutofacOwinMvc/Startup.cs
@@ -1,46 +1,51 @@
-﻿using Autofac;
-using Autofac.Integration.Owin;
+﻿using System.Reflection;
+using System.Web.Http;
+using Autofac;
 using Autofac.Integration.Mvc;
 using Autofac.Integration.WebApi;
+using AutofacOwinMvc.Example;
 using Microsoft.Owin;
 using Owin;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Web;
-using AutofacOwinMvc.Example;
-using System.Web.Http;
 
 [assembly: OwinStartup(typeof(AutofacOwinMvc.Startup))]
 
 namespace AutofacOwinMvc
 {
-  public class Startup
-  {
-    public void Configuration(IAppBuilder app) {
-      
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			var builder = new ContainerBuilder();
+			builder.RegisterControllers(Assembly.GetExecutingAssembly());
+			builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+			builder.RegisterType<ExampleService>().InstancePerRequest();
+			builder.RegisterType<ExampleMiddleware>().InstancePerRequest();
+			var container = builder.Build();
 
-      var builder = new ContainerBuilder();
+			// When using OWIN, you don't use GlobalConfiguration - you create
+			// an HttpConfiguration and set it up independently.
+			var config = new HttpConfiguration();
+			config.Routes.MapHttpRoute(
+									name: "DefaultApi",
+									routeTemplate: "api/{controller}/{id}",
+									defaults: new { id = RouteParameter.Optional }
+									);
 
+			// I didn't see anywhere that the WebAPI dependency resolver was getting
+			// set - don't forget to do this.
+			config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
 
+			// Set the MVC dependency resolver as well.
+			System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
 
-      builder.RegisterControllers(Assembly.GetExecutingAssembly());
-      builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
-
-      builder.RegisterType<ExampleService>().InstancePerRequest();
-      builder.RegisterType<ExampleMiddleware>().InstancePerRequest();
-
-
-
-
-
-      var container = builder.Build();
-
-      System.Web.Mvc.DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
-      app.UseAutofacMvc();
-      app.UseAutofacMiddleware(container);
-      app.UseAutofacWebApi(GlobalConfiguration.Configuration);
-    }
-  }
+			// OWIN middleware *order* is very important. You're setting up a
+			// pipeline and first registered is first to execute. The order was
+			// previously wrong, and the "app.UseWebApi(config)" was missing,
+			// so WebAPI didn't know it was supposed to run through OWIN.
+			app.UseAutofacMiddleware(container);
+			app.UseAutofacWebApi(config);
+			app.UseAutofacMvc();
+			app.UseWebApi(config);
+		}
+	}
 }
